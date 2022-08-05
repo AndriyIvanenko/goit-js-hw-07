7901b0620dfa860d05c2004a8d51657e98ff1cb3
Andriy Ivanenko <andriy.ivanenko@gmail.com>
2022-08-05 18:16:20 +0300

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+let galleryMarkup = "";
 
-console.log(galleryItems);
+galleryItems.forEach((galleryItem) => {
+  galleryMarkup += `<div class="gallery__item">
+  <a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</div>`;
+});
+
+gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+gallery.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const originalImgUrl = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${originalImgUrl}" width="800" height="600">
+`);
+  instance.show();
+
+  window.addEventListener("keydown", onEscPress);
+  function onEscPress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
